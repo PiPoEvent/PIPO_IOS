0474664a2637cc1b2442f5c509f37259b049be36
Thang Cao Hoang <chthang@admins-mac-mini-5.local>
2016-12-05 07:51:41 +0700

    Initial Commit

---
+++ PIPO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PIPO
+//
+//  Created by Thang Cao Hoang on 05/12/2016.
+//  Copyright © 2016 Thang Cao Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
